8f2b0ad707c2472bccbaaae5cda2a3cd7decf1cb
DONEBROS\Dan.Ashurst <dan.ashurst@betfred.com>
2019-09-11 14:49:28 +0100

    Added functionality to add items to the list

--- src/js/components/todo.js
+++ src/js/components/todo.js
@@ -1,5 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Todo = () => <div>Hello World</div>;
+const mockList = [
+ 'feed the cat',
+ 'take the rubbish out',
+ 'pay the milk bill'
+]
+
+const Todo = () => {
+    const [list, setList] = useState(mockList);
+    const [newItemText, setNewItemText] = useState('');
+
+    const updateNewItemText = e => {
+        setNewItemText(e.target.value);
+    }
+
+    const addItemToList = () => {
+        setList([
+            ...list,
+            newItemText
+        ]);
+        setNewItemText('');
+    }
+
+    return (
+        <div>
+            {
+                list.map( l => {
+                    return <div>{l}</div>
+                })
+            }
+            <input 
+                value={newItemText}
+                onChange={updateNewItemText}
+                />
+            <button 
+            onClick={addItemToList}>Add Item</button>
+        </div> 
+    );
+}
 
 export default Todo;
